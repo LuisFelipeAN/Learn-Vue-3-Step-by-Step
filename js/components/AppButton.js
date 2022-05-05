50e42bbfd61d0f264b95c52de44510391f59fff6
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-05-05 16:35:29 -0300

    episode 6

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,10 +1,20 @@
 export default{
     template:`
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded p-3 disabled:cursor-not-allowed" :disabled="processing"><slot/></button>
+        <button 
+        :class="{
+            'border rounded p-3 disabled:cursor-not-allowed': true,
+            'bg-gray-200 hover:bg-gray-400' : type=='muted',
+            'bg-blue-200 hover:bg-blue-400' : type=='primary',
+            'bg-purple-200 hover:bg-purple-400' : type=='secondary',
+            'is-loading': processing
+
+        }" :disabled="processing" ><slot/></button>
     `,
     data(){
-        return{
-            processing:true
-        }
-    }
+       return {}
+    },
+    props:{
+        type:{ type: String, default: 'primary'},
+        processing:{ type: Boolean, default: false}
+    },
 }
