ffc62281f102bc15897011cb03afdef671d7caa8
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-05-05 16:02:38 -0300

    episode 5

---
+++ js/components/AppButton.js
@@ -0,0 +1,10 @@
+export default{
+    template:`
+        <button class="bg-gray-200 hover:bg-gray-400 border rounded p-3 disabled:cursor-not-allowed" :disabled="processing"><slot/></button>
+    `,
+    data(){
+        return{
+            processing:true
+        }
+    }
+}
